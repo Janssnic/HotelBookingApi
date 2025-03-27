c57b8b77eb20435a3dd1616e056f44ca17718b25
janssnic <janssnic@arcada.fi>
2025-03-27 13:34:04 +0000

    list of dicts

--- roomBooking/main.py
+++ roomBooking/main.py
@@ -7,23 +7,23 @@ PORT=8212
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-rooms = {
-    101: {"room No.": 101, "type": "single", "status": "available"},
-    102: {"room No.": 102, "type": "double", "status": "occupied"},
-    103: {"room No.": 103, "type": "single", "status": "available"},
-    104: {"room No.": 104, "type": "single", "status": "available"},
-    105: {"room No.": 105, "type": "double", "status": "occupied"},
-    106: {"room No.": 106, "type": "single", "status": "available"},
-    107: {"room No.": 107, "type": "single", "status": "available"},
-    208: {"room No.": 208, "type": "double", "status": "occupied"},
-    209: {"room No.": 209, "type": "single", "status": "available"},
-    210: {"room No.": 210, "type": "single", "status": "available"},
-    211: {"room No.": 211, "type": "double", "status": "occupied"},
-    301: {"room No.": 301, "type": "suite", "status": "available"},
-    303: {"room No.": 303, "type": "suite", "status": "available"},
-    305: {"room No.": 305, "type": "suite", "status": "available"},
-    307: {"room No.": 307, "type": "suite", "status": "available"},
-}
+rooms = [
+    {"room No.": 101, "type": "single", "status": "available"},
+    {"room No.": 102, "type": "double", "status": "occupied"},
+    {"room No.": 103, "type": "single", "status": "available"},
+    {"room No.": 104, "type": "single", "status": "available"},
+    {"room No.": 105, "type": "double", "status": "occupied"},
+    {"room No.": 106, "type": "single", "status": "available"},
+    {"room No.": 107, "type": "single", "status": "available"},
+    {"room No.": 208, "type": "double", "status": "occupied"},
+    {"room No.": 209, "type": "single", "status": "available"},
+    {"room No.": 210, "type": "single", "status": "available"},
+    {"room No.": 211, "type": "double", "status": "occupied"},
+    {"room No.": 301, "type": "suite", "status": "available"},
+    {"room No.": 303, "type": "suite", "status": "available"},
+    {"room No.": 305, "type": "suite", "status": "available"},
+    {"room No.": 307, "type": "suite", "status": "available"},
+]
 
 @app.get("/rooms")
 def read_root():
